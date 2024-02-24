76c1d0d36704bec133b9d1a9b12ee0a21ad17fa7
webplaner <webplan@me.com>
2024-02-25 00:12:09 +0900

    a

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -9,13 +11,56 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int number = 1;
+  String imgname = 'brush';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lime,
       body: Center(
-        child: Image.asset('asset/images/brush.png')
+        child: GestureDetector(
+          onTap: (){
+            final randnumber = Random().nextInt(9);
+            setState(() {
+              number = randnumber + 1;
+              switch(number)
+              {
+                case 9:
+                  imgname = 'delete';
+                  break;
+                case 8:
+                  imgname = 'color';
+                  break;
+                case 7:
+                  imgname = 'edit';
+                  break;
+                case 6:
+                  imgname = 'erase';
+                  break;
+                case 5:
+                  imgname = 'pencil';
+                  break;
+                case 4:
+                  imgname = 'paint-bucket';
+                  break;
+                case 3:
+                  imgname = 'paint';
+                  break;
+                case 3:
+                  imgname = 'undo';
+                  break;
+                default :
+                  imgname = 'brush';
+              }
+              print(number);
+            });
+
+          },
+          child: Image.asset(
+              'asset/images/$imgname.png'
+          ),
+        ),
+
       )
     );
   }
